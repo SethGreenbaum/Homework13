fa2e3b1b9b17deb40b617f48486d90f829edf81e
Seth Greenbaum <sethmgreenbaum@gmail.com>
2020-04-22 10:53:48 -0600

    added PWA elements and IndexedDb code

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+var PORT = process.env.PORT || 8080;
 
 const app = express();
 
@@ -14,8 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
